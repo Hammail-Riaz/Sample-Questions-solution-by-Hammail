b2c8f6e92976c939cce82663d745d976a550daa1
Hammail <hammailriaz.dev@gmail.com>
2026-02-01 09:34:01 -0800

    Implement prime_in_range function to find primes
    Refactor prime number function and improve logic.

--- Question No 8.py
+++ Question No 8.py
@@ -1,12 +1,25 @@
 # Qno8: Create a function that takes two numbers (start and end) and returns a list of all prime numbers between them.
 
 # sol no 8:
-# def prime_number_in_range(x, y):
-#     is_prime = None
-#     prime_numbers = []
-#     for number in range(x, y+1):
-#         for i in number:
-#             if i%number:
+def prime_in_range(x, y):
+    """
+    return a list containing prime number in the given range (x, y)
+    """
+    prime_numbers = []
+    for number in range(x, y+1):
+        if number > 1: # prime number always start from 2
+            is_prime = True
+            for count in range(2, int(number**0.5)+1):
+                if number % count == 0: #checking each possible factor of the current iteration number
+                    is_prime = False
+                    break
+                
+            if is_prime:
+                prime_numbers.append(number) #adding the prime number in list
+            
+    return prime_numbers
+        
 
-
-# Still Remaining                
+for num in prime_in_range(1, 50):
+    print(num)
+    
